fc92ea8b24c9b30a7fd2e137c0b4ed57559a588d
Native19 <Native095@yandex.ru>
2019-10-16 14:53:07 +1000

    Add files via upload

---
+++ AirTravelPlanning/Interfaces/IRepisitory.cs
@@ -0,0 +1,20 @@
+﻿using AirTravelPlanning.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AirTravelPlanning.Interfaces
+{
+    public interface IRepisitory
+    {
+        List<RouteModel> LoadData();
+        RouteModel AddRoute();
+
+        void DeleteRoute();
+
+        void DeleteFlight();
+
+        void UpdateRoute();
+        void UdateFlight();
+    }
+}
---
+++ AirTravelPlanning/Repositories/DataRepository.cs
@@ -0,0 +1,16 @@
+﻿using AirTravelPlanning.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AirTravelPlanning.Repositories
+{
+    public static class DataRepository : IRepisitory
+    {
+        public List<RouteModel> LoadData ()
+        {
+
+        }
+ 
+    }
+}
